ece7b7ee9ceabaeae72bd7458b5bf2bc63ab608a
hz <hz15858@163.com>
2022-12-04 18:16:07 +0800

    fix blog

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
  * @Author: hz hz15858@163.com
  * @Date: 2022-12-03 14:24:46
  * @LastEditors: hz hz15858@163.com
- * @LastEditTime: 2022-12-04 14:26:45
+ * @LastEditTime: 2022-12-04 15:56:55
  * @FilePath: /vuetest/src/api/api.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -29,5 +29,15 @@ export function postList(param){
     //         page: param.page,
     //         per_page: param.per_page
     //     }
+    // })
+}
+
+export function getPostListByOther(params){
+    return service.get('postController/getPostListByOther',{params: params|| {}})
+
+    // return service({
+    //     method: 'get',
+    //     url: 'postController/getPostListByOther',
+    //     params
     // })
 }
--- src/components/post/PostList.vue
+++ src/components/post/PostList.vue
@@ -2,12 +2,23 @@
  * @Author: hz hz15858@163.com
  * @Date: 2022-12-03 15:28:35
  * @LastEditors: hz hz15858@163.com
- * @LastEditTime: 2022-12-04 14:45:47
+ * @LastEditTime: 2022-12-04 16:44:03
  * @FilePath: /vuetest/src/components/post/PostList.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <div class="postList">
+    <el-form :inline="true" :model="formInline" class="demo-form-inline" size="small">
+    <el-form-item label="姓名">
+        <el-input v-model="formInline.name" placeholder="请输入姓名查询"></el-input>
+    </el-form-item>
+    <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+    </el-form-item>
+    <el-form-item>
+        <el-button type="primary" @click="onReset">重置</el-button>
+    </el-form-item>
+    </el-form>
     <el-table :data="tableData" v-loading= "loading" border style="width: 100%">
         <el-table-column prop="postId" label="文档Id" align="center"></el-table-column>
         <el-table-column prop="authorId" label="作者Id" align="center"></el-table-column>
@@ -26,13 +37,35 @@
         <el-table-column label="操作" align="center">
             <template slot-scope="scope">
                 <el-button type="danger" size="mini" icon="el-icon-delete" @click="del(scope.postId)"></el-button>
-                <el-button type="danger" size="mini" icon="el-icon-update" @click="del(scope.postId)"></el-button>
+                <el-button type="danger" size="mini" icon="el-icon-edit" @click="updatePostMessage"></el-button>
+                
             </template>
         </el-table-column>
-
-
-
     </el-table>
+
+    <el-dialog title="修改博客信息" :visible.sync="dialogFormVisible">
+        <el-form :model="form">
+            <el-form-item label="活动名称" :label-width="formLabelWidth">
+            <el-input v-model="form.name" autocomplete="off"></el-input>
+            </el-form-item>
+
+            <el-form-item label="活动名称" :label-width="formLabelWidth">
+            <el-input v-model="form.name" autocomplete="off"></el-input>
+            </el-form-item>
+
+            <el-form-item label="活动名称" :label-width="formLabelWidth">
+            <el-input v-model="form.name" autocomplete="off"></el-input>
+            </el-form-item>
+
+            <el-form-item label="活动名称" :label-width="formLabelWidth">
+            <el-input v-model="form.name" autocomplete="off"></el-input>
+            </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+        </div>
+        </el-dialog>
 
     <el-pagination
       @size-change="handleSizeChange"
@@ -47,8 +80,9 @@
 </template>
 
 <script>
-import { postList }  from '@/api/api'
+import { postList,getPostListByOther }  from '@/api/api'
 import { del } from 'vue'
+import { Alert } from 'element-ui'
 export default {
     data() {
       return {
@@ -73,7 +107,15 @@ export default {
         page:1,
         per_page:10,
         total:0,
-        loading:false
+        loading:false,
+        formInline:{
+            name:''
+        },
+        dialogFormVisible:false,
+        form:{
+            name: ''
+        },
+        formLabelWidth: '120px'
       }
     },
     created(){
@@ -90,19 +132,47 @@ export default {
                 }
             })
         },
+        getDataByOther(){
+            getPostListByOther({page: this.page,per_page:this.per_page,title:this.formInline.name})
+            .then(res =>{
+                if(res.code === 100000){
+                    this.total = res.data.totalSize
+                    this.tableData =res.data.data
+                    //loading = false
+                }
+            })
+        },
         handleSizeChange(val) {
             console.log(`每页 ${val} 条`);
             this.per_page =val;
             this.page =1
-            this.getData({page: this.page,per_page:this.per_page})
+            if(this.formInline.name !==''){
+                this.getDataByOther()
+            }else{
+                this.getData({page: this.page,per_page:this.per_page})
+            }
         },
         handleCurrentChange(val) {
             console.log(`当前页: ${val}`);
             this.page =val
+
+            if(this.formInline.name !==''){
+                this.getDataByOther()
+            }else{
             this.getData({page: this.page,per_page:this.per_page})
+            }
         },
         del(){
-
+            
+        },
+        onSubmit(){
+            this.getDataByOther()
+        },
+        onReset(){
+            this.getData({page: this.page,per_page:this.per_page})
+        },
+        updatePostMessage(){
+            this.dialogFormVisible =true
         }
     }
   }
@@ -113,6 +183,13 @@ export default {
     .el-pagination{
         text-align: left;
         margin-top: 20px;
+    }
+    .demo-form-inline,.el-form-item{
+        text-align: left;
+    }
+    .el-dialog{
+        width: 500px;
+        text-align: left;
     }
 }
 </style>
